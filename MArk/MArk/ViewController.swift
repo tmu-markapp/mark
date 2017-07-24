7f2f70afc19113f59a93160079086ab62ee23410
Shota-Nemoto <em.shtkrass@icloud.com>
2017-07-24 18:51:09 +0900

    Mark_Project

---
+++ MArk/MArk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MArk
+//
+//  Created by shota nemoto on 2017/07/24.
+//  Copyright © 2017 shota nemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
